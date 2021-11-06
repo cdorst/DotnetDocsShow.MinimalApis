fa690a076f7a6834334433a3e0528d258112100b
Christopher Dorst <chris@venassure.com>
2021-11-06 04:35:07 -0500

    Return Mediator.Send Task without allocating additional Task from async/await keywords

--- DotnetDocsShow.Structured.Mediator/Program.cs
+++ DotnetDocsShow.Structured.Mediator/Program.cs
@@ -11,9 +11,9 @@ builder.Services.AddMediatR(typeof(Customer));
 
 var app = builder.Build();
 
-app.MapGet("customers", async (IMediator mediator) => await mediator.Send(new GetAllCustomersRequest()));
-app.MapGet("/customers/{id}", async (IMediator mediator, Guid id) => await mediator.Send(new GetCustomerByIdRequest(id)));
-app.MapPost("/customers", async (IMediator mediator, CreateCustomerRequest request) => await mediator.Send(request));
-app.MapDelete("/customers/{id}", async (IMediator mediator, Guid id) => await mediator.Send(new DeleteCustomerByIdRequest(id)));
+app.MapGet("customers", (IMediator mediator) => mediator.Send(new GetAllCustomersRequest()));
+app.MapGet("/customers/{id}", (IMediator mediator, Guid id) => mediator.Send(new GetCustomerByIdRequest(id)));
+app.MapPost("/customers", (IMediator mediator, CreateCustomerRequest request) => mediator.Send(request));
+app.MapDelete("/customers/{id}", (IMediator mediator, Guid id) => mediator.Send(new DeleteCustomerByIdRequest(id)));
 
 app.Run();
